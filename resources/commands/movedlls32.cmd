6f1f7833e1cac48e9c597c961a09aa6319d48eef
SuperCdat <126152622+SuperCdat@users.noreply.github.com>
2024-06-15 22:22:39 +0700

    Delete resources directory

--- resources/commands/movedlls32.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-
-cd resources\dlls\X86\
-takeown /F "C:\Windows\System32\Windows.ApplicationModel.Store.dll"
-copy "System32\Windows.ApplicationModel.Store.dll" "C:\Windows\System32"
-
-exit
